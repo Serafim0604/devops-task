d1d0c21767e485d1368123f6a9bc865781d489a8
Serafim <serafim0604@gmail.com>
2025-04-04 20:39:48 +0700

    Removed inventory.yaml. Implemented inventory generation.

--- deploy.py
+++ deploy.py
@@ -4,6 +4,19 @@ import argparse
 import os
 
 
+def generate_inventory(ip, ssh_key_path):
+    inventory_content = (
+        "[postgres]\n"
+        f"{ip} ansible_user=root ansible_ssh_private_key_file={ssh_key_path}\n"
+    )
+
+    with open("inventory.ini", "w") as f:
+        f.write(inventory_content.strip())
+
+    print("[*] Generated inventory.ini:")
+    print(inventory_content.strip())
+
+
 def main():
     parser = argparse.ArgumentParser(description="Parse IPs string")
     parser.add_argument("hosts", help="List of IPs separated by comma, example: 192.168.56.105,192.168.56.105")
@@ -11,13 +24,24 @@ def main():
     args = parser.parse_args()
 
     ssh_key_path = os.path.expanduser(args.key)
+    if not os.path.exists(ssh_key_path):
+        print(f"[!] SSH key not found: {ssh_key_path}")
+        exit(1)
+
+    print(f"[*] SSH-key path: {ssh_key_path}")
+
     hosts = [ip.strip() for ip in args.hosts.split(',')]
+    if len(hosts) != 2:
+        print(f"[!] Expected 2 IP addresses: {len(hosts)}")
+        exit(1)
 
     print("[*] Got IP addresses:")
     for ip in hosts:
         print(f"  - {ip}")
-    
-    print(f"[*] SSH-key path: {ssh_key_path}")
+
+    selected_host = hosts[0]
+    generate_inventory(selected_host, ssh_key_path)
+
 
 if __name__ == "__main__":
     main()
